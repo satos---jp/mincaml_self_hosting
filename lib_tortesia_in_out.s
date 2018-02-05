e9240e19172c5601aa805b82649dc7c1ba5de512
satos---jp <syogo8118@yahoo.co.jp>
2018-02-05 21:49:47 +0900

    fix tortesia in out

--- lib_tortesia_in_out.s
+++ lib_tortesia_in_out.s
@@ -1,13 +1,32 @@
 read_int:
-	ini r5
+	in r5
+	in r6
+	sll r6,r6,$8
+	or r5,r5,r6
+	in r6
+	sll r6,r6,$16
+	or r5,r5,r6
+	in r6
+	sll r6,r6,$24
+	or r5,r5,r6
 	ret
 
 read_float:
-	inf r5
+	in r5
+	in r6
+	sll r6,r6,$8
+	or r5,r5,r6
+	in r6
+	sll r6,r6,$16
+	or r5,r5,r6
+	in r6
+	sll r6,r6,$24
+	or r5,r5,r6
 	rtof f5,r5
 	ret
 
 print_char:
 	out r10
 	ret
+
 
