950e3cd74bc3a0ed744770022eb21bbc5d36fd71
satos---jp <syogo8118@yahoo.co.jp>
2018-02-24 19:10:10 +0900

    fix file positions.

--- scripts/compile_exec.sh
+++ scripts/compile_exec.sh
@@ -1,5 +1,5 @@
 ./main $1 -t -o o_tortesia.s -noinline -asi -v > o.txt && 
-python tortesia2x86.py -r $2 < o_tortesia.s > out.s && 
+python scripts/tortesia2x86.py -r $2 < o_tortesia.s > out.s && 
 if [ -z $2 ]; then
 	./scripts/exec.sh
 else
